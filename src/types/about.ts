4d5ac49a01c0116971a70f729607ed59ac0cb746
mels <52806683+melsykin@users.noreply.github.com>
2024-11-28 13:05:49 -0800

    Update src/types/about.ts [BoltSync]

---
+++ src/types/about.ts
@@ -0,0 +1,12 @@
+import { LucideIcon } from 'lucide-react';
+
+export interface Interest {
+  icon: LucideIcon;
+  title: string;
+  text: string;
+}
+
+export interface Skill {
+  name: string;
+  level: number;
+}
