6ee180bfcb232cb90b3146eece9f9bc8ea7cd83f
mels <52806683+melsykin@users.noreply.github.com>
2024-11-28 13:05:47 -0800

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import AboutMe from './components/AboutMe';
+import NavBar from './components/layout/NavBar';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import { ROUTES } from './constants';
 
 function App() {
   return (
@@ -10,8 +12,9 @@ function App() {
       <div className="min-h-screen bg-slate-50">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.ABOUT} element={<AboutMe />} />
+          <Route path={ROUTES.PROJECTS} element={<Projects />} />
         </Routes>
       </div>
     </Router>
